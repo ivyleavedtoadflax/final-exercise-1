0b1b63d6548efc2f7e731e6c9440a9cc96fb57ce
Matthew Upson <ivyleavedtoadflax@gmail.com>
2014-02-04 08:47:58 -0800

    Added sort columns bash

--- print_col_names.py
+++ print_col_names.py
@@ -11,10 +11,3 @@ col_names_nw = []
 
 for i in col_names:
     print(re.sub(" ","",i))
-
-# STEP 3 - Using the shell:
-# (a) write ONLY the new non-whitespace column names to a new file called columnNames instead of the shell output (stdout)
-# (b) similarly, sort the outputted column names into a new file called sortedColumnNames
-
-# Push the following files to Aleksandra's github: this file containing the fixed code, file with the column names with spaces removed, and the file with the sorted names
-# This will raise a pull request, so we can check your script
